780565e03208b46b496f23584bb8996ee06d5702
David Golden <dagolden@cpan.org>
2011-01-28 09:37:30 -0500

    convert to dzil

--- lib/Tee/App.pm
+++ lib/Tee/App.pm
@@ -1,20 +1,14 @@
-# Copyright (c) 2008 by David Golden. All rights reserved.
-# Licensed under Apache License, Version 2.0 (the "License").
-# You may not use this file except in compliance with the License.
-# A copy of the License was distributed with this file or you may obtain a 
-# copy of the License from http://www.apache.org/licenses/LICENSE-2.0
-
-package Tee::App;
 use strict;
+use warnings;
+package Tee::App;
+# ABSTRACT: Implementation of ptee
+
 use Exporter ();
 use File::Basename qw/basename/;
 use Getopt::Long;
 use IO::File;
-
-@Tee::App::ISA = 'Exporter';
-@Tee::App::EXPORT = qw/run/;
-$Tee::App::VERSION = '0.13_51';
-$Tee::App::VERSION = eval $Tee::App::VERSION; ## no critic
+our @ISA = 'Exporter';
+our @EXPORT = qw/run/;
 
 #--------------------------------------------------------------------------#
 # define help text
@@ -102,14 +96,6 @@ __END__
 
 =begin wikidoc
 
-= NAME
-
-Tee::App - Pure Perl emulation of GNU tee
-
-= VERSION
-
-This documentation refers to version %%VERSION%%
-
 = DESCRIPTION
 
 Guts of the {ptee} command.
@@ -126,29 +112,6 @@ Bugs can be submitted through the web interface at
 
 When submitting a bug or request, please include a test-file or a patch to an
 existing test-file that illustrates the bug or desired feature.
-
-= AUTHOR
-
-David A. Golden (DAGOLDEN)
-
-= COPYRIGHT AND LICENSE
-
-Copyright (c) 2006-2008 by David A. Golden. All rights reserved.
-
-Licensed under Apache License, Version 2.0 (the "License").
-You may not use this file except in compliance with the License.
-A copy of the License was distributed with this file or you may obtain a 
-copy of the License from http://www.apache.org/licenses/LICENSE-2.0
-
-Files produced as output though the use of this software, shall not be
-considered Derivative Works, but shall be considered the original work of the
-Licensor.
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
 
 =end wikidoc
 
